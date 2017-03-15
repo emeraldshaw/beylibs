9831061e7740b8916fbd41c1446884782ba8c1fd
Emerald <eshaw589@gmail.com>
2017-03-15 18:23:11 -0400

    update

--- drunkinlove/scripts/module/module.js
+++ drunkinlove/scripts/module/module.js
@@ -3,9 +3,9 @@ var app = angular.module('beylibsApp', ['ngRoute']);
 app.config(function($routeProvider, $locationProvider) {
 
   $routeProvider
-    .when('/', {
-      controller: '',
-      templateUrl: ''
+    .when('/userinput', {
+      controller: 'inputCtrl',
+      templateUrl: 'views/form.html'
     })
     .when('/', {
       controller: '',
